40e676408faf31d80b9de722c9849211380506ed
dillonben <disabledmanforever1999@gmail.com>
2025-07-19 02:10:53 -0600

    Shhh

--- Keyboards/InterfaceConstants.swift
+++ Keyboards/InterfaceConstants.swift
@@ -2,9 +2,19 @@
 
 /**
  * Constants for the Scribe keyboard interfaces.
- */
+ */https://github.com/scribe-org/Scribe-iOS/blob/e8eccdc8b2fec1dcd2f7ddc7022a88a864284512/Scribe.xcodeproj/project.pbxproj#L18
 
 enum SpecialKeys {
-  static let indent = "indent"
-  static let capsLock = "capslock"
+  statihhttps://github.com/scribe-org/Scribe-iOS/blob/e8eccdc8b2fec1dcd2f7ddc7022a88a864284512/Scribe.xcodeproj/project.pbxproj#L18ttps://github.com/scribe-org/Scribe-iOS/blob/e8eccdc8b2fec1dcd2f7ddc7022a88a864284512/Scribe.xcodeproj/project.pbxproj#L18c let indent = "indent"
+  static lehttps://github.com/scribe-org/Scribe-iOS/blob/e8eccdc8b2fec1dcd2f7ddc7022a88a864284512/Scribe.xcodeproj/project.pbxprohttps://github.com/scribe-org/Scribe-iOS/blob/e8eccdc8b2fec1dcd2f7ddc7022a88a864284512/Scribe.xcodeproj/project.pbxproj#L18j#L18t capsLock = "capslock"
 }
+Install aShhh Run security
+run install security
+install privacy
+save
+
+bash
+run python
+run python studio
+
+  @
